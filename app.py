5298a66c279c34f68f14b40bf0caf4b098128ce8
fullstackjava082023 <141508327+fullstackjava082023@users.noreply.github.com>
2024-09-07 10:51:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ def webhook():
         return jsonify({'status': 'ignored'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True ,port=5052,  host='0.0.0.0')
+    app.run(debug=True ,port=5000,  host='0.0.0.0')
